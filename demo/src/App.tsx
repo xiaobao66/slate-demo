d79cca8a5a2791ed7bc1072ef859c2db9d22679b
xiaobaowei <xiaobaowei@tencent.com>
2022-05-25 20:38:03 +0800

    feat: add editor render

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,10 +2,21 @@ import React, { useState } from 'react'
 import { createEditor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]
+
 function App() {
   const [editor] = useState(() => withReact(createEditor()))
 
-  return null
+  return (
+    <Slate editor={editor} value={initialValue}>
+      <Editable />
+    </Slate>
+  )
 }
 
 export { App }
